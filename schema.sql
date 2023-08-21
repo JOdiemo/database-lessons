e65366682f460709bdd2f06192e1f911f6970efc
Jorum Odiemo <odiemo@gmail.com>
2023-08-21 23:32:33 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,11 +1,13 @@
-/* Database schema to keep the structure of entire database. */
+/* Database schema to keep the structure of the entire database. */
+
 
 CREATE TABLE animals(
-   id INT,
+   id serial primary key,
    name CHAR(20),
    date_of_birth DATE,
    escape_attempt INT,
    neutered BOOL,
    weight_kg DECIMAL   
 ); 
+    
 
