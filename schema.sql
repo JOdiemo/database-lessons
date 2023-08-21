cfc5415fd1be9ae06dc6e555679cc527a726dfc1
Jorum Odiemo <odiemo@gmail.com>
2023-08-21 14:56:45 +0300

    Update schema.sql
    Create the animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals(
+   id INT,
+   name CHAR(20),
+   date_of_birth DATE,
+   escape_attempt INT,
+   neutered BOOL,
+   weight_kg DECIMAL   
+); 
+
