01e47d0ad3996a179bbb1f9d9840760bdc5c66f4
Jorum Odiemo <odiemo@gmail.com>
2023-08-23 04:50:32 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -13,3 +13,41 @@ CREATE TABLE animals(
 ALTER TABLE animals
     ADD species varchar(20);   
 
+/*Day Three*/
+
+CREATE TABLE owners(
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(50),
+  age INT  
+);
+SELECT * FROM owners
+
+CREATE TABLE species(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50)  
+);
+SELECT * FROM species
+
+ALTER TABLE animals
+    ADD COLUMN IF NOT EXISTS owner_id
+	INT;
+	
+ALTER TABLE animals
+	ADD CONSTRAINT fk_animals_species
+	FOREIGN KEY (species_id)
+	REFERENCES species(id)
+	ON DELETE CASCADE;	
+
+select * from animals
+
+ALTER TABLE animals
+    ADD COLUMN IF NOT EXISTS owner_id
+	INT;
+	
+ALTER TABLE animals
+	ADD CONSTRAINT fk_animals_owners
+	FOREIGN KEY (owner_id)
+	REFERENCES owners(id)
+	ON DELETE CASCADE;	
+
+select * from animals
