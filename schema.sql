1a86ed8716fc164eb2965413646bb8502666c8b0
Jorum Odiemo <odiemo@gmail.com>
2023-08-24 10:34:02 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -51,3 +51,36 @@ ALTER TABLE animals
 	ON DELETE CASCADE;	
 
 select * from animals
+
+/*Day Four*/
+--Create a table named vets with the following columns
+CREATE TABLE vets(
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(50),
+   age INT,
+   date_of_graduation DATE
+);
+
+SELECT * FROM vets
+
+--Create bridge table specializations
+CREATE TABLE specializations (
+	vet_id INT NOT NULL, 
+	species_id INT NOT NULL, 
+	PRIMARY KEY (vet_id, species_id),
+	CONSTRAINT fk_vet FOREIGN KEY (vet_id) 
+	REFERENCES vets(id),
+	CONSTRAINT fk_species FOREIGN KEY(species_id) 
+	REFERENCES species(id));
+SELECT * FROM specializations
+	
+--Create bridge table visits	
+CREATE TABLE visits (
+	vet_id INT NOT NULL, 
+	animals_id INT NOT NULL, 
+	PRIMARY KEY (vet_id, animals_id),
+	CONSTRAINT fk_vet FOREIGN KEY (vet_id) 
+	REFERENCES vets(id),
+	CONSTRAINT fk_animals FOREIGN KEY(animals_id) 
+	REFERENCES animals(id));
+SELECT * FROM visits
