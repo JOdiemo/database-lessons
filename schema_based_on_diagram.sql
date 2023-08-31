4ef677964a4ea27d0d29d8d8fe2a36dd6d0a74b6
aqurds <omarf1320@gmail.com>
2023-08-31 14:43:20 +0600

    Add schema in diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,3 +1,49 @@
+/* Database schema to keep the structure of the entire database. */
+CREATE TABLE animals(
+   id serial primary key,
+   name CHAR(20),
+   date_of_birth DATE,
+   escape_attempt INT,
+   neutered BOOL,
+   weight_kg DECIMAL   
+); 
+
+CREATE TABLE owners(
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(50),
+  age INT  
+);
+
+CREATE TABLE species(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50)  
+);
+
+CREATE TABLE vets(
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(50),
+   age INT,
+   date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+	vet_id INT NOT NULL, 
+	species_id INT NOT NULL, 	
+	CONSTRAINT fk_vet FOREIGN KEY (vet_id) 
+	REFERENCES vets(id),
+	CONSTRAINT fk_species FOREIGN KEY(species_id) 
+	REFERENCES species(id));
+	
+CREATE TABLE visits (
+	vet_id INT NOT NULL, 
+	animals_id INT NOT NULL, 
+	date_of_visit DATE,	
+	CONSTRAINT fk_vet FOREIGN KEY (vet_id) 
+	REFERENCES vets(id),
+	CONSTRAINT fk_animals FOREIGN KEY(animals_id) 
+	REFERENCES animals(id));
+
+
 --Create a file named schema_based_on_diagram.sql where you implement the database from the diagram.
 --Join tables from many-to-many relationships might not appear in the diagram, but you still need them.
 --Remember to add the FK indexes.
