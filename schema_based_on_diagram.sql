2249d7c7d41131a2bd0583930faf35cc9e265e25
Jorum Odiemo <odiemo@gmail.com>
2023-08-31 11:10:29 +0300

    Create schema_based_on_diagram.sql

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,7 @@
+--Create a file named schema_based_on_diagram.sql where you implement the database from the diagram.
+--Join tables from many-to-many relationships might not appear in the diagram, but you still need them.
+--Remember to add the FK indexes.
+
+--Data Table Schema is at the below link
+
+https://lucid.app/lucidchart/034f07a9-ac76-448c-a339-aa7dfdeb0d38/edit?viewport_loc=-12136%2C-11167%2C1916%2C830%2C0_0&invitationId=inv_789b487f-c1ec-4a01-9545-ea7ae10fe1d7
